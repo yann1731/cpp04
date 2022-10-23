56223b7eb6f4bd5c0cabe7ade4ef085ee2b778f1
fenix144 <yan_1731@hotmail.com>
2022-10-23 13:56:06 -0400

    let's go

--- ex01/Brain.cpp
+++ ex01/Brain.cpp
@@ -1,2 +1,33 @@
 #include "Brain.hpp"
 
+Brain::Brain ()
+{
+	for (int i = 0; i < 100; i++)
+		this->ideas[i] = "food";
+};
+
+Brain::Brain (const std::string idea)
+{
+	for (int i = 0; i < 100; i++)
+		this->ideas[i] = idea;
+};
+
+Brain::Brain (const Brain& brain)
+{
+	for (int i = 0; i < 100; i++)
+		this->ideas[i] = brain.ideas[i];
+};
+
+Brain Brain::operator= (const Brain& brain)
+{
+	Brain newBrain;
+
+	for (int i = 0; i < 100; i++)
+		newBrain.ideas[i] = brain.ideas[i];
+	return (newBrain);
+};
+
+Brain::~Brain ()
+{
+	return ;
+};
