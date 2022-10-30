a87c2251e793e17d8fddbcb2cfe5f106d31467b7
fenix144 <yan_1731@hotmail.com>
2022-10-30 13:41:00 -0400

    let's go

--- ex03/AMateria.hpp
+++ ex03/AMateria.hpp
@@ -1,10 +1,14 @@
 #pragma once
 #include "ICharacter.hpp"
 
+// Complete the definition of the following AMateria class and implement the necessary member functions
+// Implement the Materias concrete classes Ice and Cure. Use their name in lower-case ("ice" for Ice, "cure" for Cure) to set their types.
+// Of course, their member function clone() will return a new instance of the same type (i.e., if you clone an Ice Materia,
+// you will get a new Ice Materia).
 class AMateria
 {
 	protected:
-	//[...]
+	std::string type;
 	public:
 	AMateria(std::string const & type);
 	//[...]
