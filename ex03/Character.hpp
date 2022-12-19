0265212b15c4e1294613a70ea7b0e5ba4e998910
fenix144 <yan_1731@hotmail.com>
2022-12-19 15:39:27 -0500

    let's go

--- ex03/Character.hpp
+++ ex03/Character.hpp
@@ -1,2 +1,26 @@
 #include "ICharacter.hpp"
 
+// class ICharacter
+// {
+// 	public:
+// 	virtual ~ICharacter() {}
+// 	virtual std::string const & getName() const = 0;
+// 	virtual void equip(AMateria* m) = 0;
+// 	virtual void unequip(int idx) = 0;
+// 	virtual void use(int idx, ICharacter& target) = 0;
+// };
+
+class Character
+{
+	public:
+	Character ();
+	Character (const std::string name);
+	~Character ();
+	
+	std::string const & getName(void) const;
+	void equip(AMateria *m);
+	void unequip(int idx);
+	void use(int idx, ICharacter & target);
+	protected:
+	std::string name;
+};
